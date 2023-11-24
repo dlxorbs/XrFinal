91c4c38f88ff293cfc9e5af3a6b3be8d5cbf4b98
Taegyun <taegyun98@naver.com>
2023-11-24 17:51:57 +0900

    FirstCommit

---
+++ style.js
@@ -0,0 +1,104 @@
+/* global AFRAME */
+
+/**
+ * Spawn entity at the intersection point on click, given the properties passed.
+ *
+ * `<a-entity intersection-spawn="mixin: box; material.color: red">` will spawn
+ * `<a-entity mixin="box" material="color: red">` at intersection point.
+//  */
+// AFRAME.registerComponent("intersection-spawn", {
+//   schema: {
+//     default: "",
+//     parse: AFRAME.utils.styleParser.parse, // 문자열 xyz의 값을 순서대로 확인
+//   },
+
+//   init: function () {
+//     var data = this.data;
+//     console.log(data);
+//     var el = this.el;
+
+//     el.addEventListener(data.event, (evt) => {
+//       // 엘리먼트를 생성할 수 있도록 제작
+//       var spawnEl = document.createElement("a-entity");
+
+//       //제작된 엘리먼트에서 position의 값을 상호작용한 포인트에 생성
+//       spawnEl.setAttribute("position", evt.detail.intersection.point);
+
+//       // Set components and properties.
+//       Object.keys(data).forEach((name) => {
+//         if (name === "event") {
+//           return;
+//         }
+//         AFRAME.utils.entity.setComponentProperty(spawnEl, name, data[name]);
+//       });
+
+//       // Append to scene.
+//       el.sceneEl.appendChild(spawnEl);
+//     });
+//   },
+// });
+
+let intersection;
+let clickPoint;
+let positionMap = document.querySelector("#positionMap");
+
+//커서 리스너 통해서 박스 생성
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    offset: { type: "vec3" },
+    snap: { type: "vec3" },
+  },
+
+  init: function () {
+    this.el.addEventListener("raycaster-intersected", (evt) => {
+      this.raycaster = evt.detail.el;
+
+      this.el.addEventListener("click", (evt) => {
+        const clickPoint = document.createElement(`a-box`);
+
+        clickPoint.setAttribute("position", evt.detail.intersection.point);
+
+        console.log(intersection);
+        console.log(clickPoint);
+        positionMap.appendChild(clickPoint);
+      });
+    });
+
+    this.el.addEventListener("raycaster-intersected-cleared", (evt) => {
+      this.raycaster = null;
+    });
+  },
+  tick: function () {
+    if (!this.raycaster) {
+      return;
+    }
+    intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+    if (!intersection) {
+      return;
+    }
+  },
+});
+
+AFRAME.registerComponent("snap", {
+  dependencies: ["position"],
+
+  schema: {
+    offset: { type: "vec3" },
+    snap: { type: "vec3" },
+  },
+
+  init: function () {
+    this.originalPos = this.el.getAttribute("position");
+  },
+
+  update: function () {
+    var data = this.data;
+
+    var pos = AFRAME.utils.clone(this.originalPos);
+    pos.x = Math.floor(pos.x / data.snap.x) * data.snap.x + data.offset.x;
+    pos.y = Math.floor(pos.y / data.snap.y) * data.snap.y + data.offset.y;
+    pos.z = Math.floor(pos.z / data.snap.z) * data.snap.z + data.offset.z;
+
+    this.el.setAttribute("position", pos);
+  },
+});
