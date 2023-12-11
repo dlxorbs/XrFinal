579d1c925bdf8f9e195098cff3446fdaaf4142ef
taegyun <taegyun98@naver.com>
2023-12-12 00:07:16 +0900

    asd

--- image.js
+++ image.js
@@ -2,7 +2,11 @@ const rockbtn = document.querySelectorAll(".rockbtn");
 const coralbtn = document.querySelectorAll(".coralbtn");
 const woodbtn = document.querySelectorAll(".woodbtn");
 const seaweedbtn = document.querySelectorAll(".seaweedbtn");
+const fishbtn = document.querySelectorAll(".fishbtn");
 
+function openModal(){
+  
+}
 // 돌 이미지
 rockbtn.forEach((element, index) => {
   // console.log(element);
@@ -28,3 +32,12 @@ seaweedbtn.forEach((element, index) => {
   element.style.backgroundImage = `url(./Img/seaweed/seaweed${number}.png)`;
 });
 
+fishbtn.forEach((element, index) => {
+  // console.log(element);
+  const number = index + 1;
+  element.style.backgroundImage = `url(./Img/fish/fish${number}.png)`;
+
+  element.addEventListener("click", (e) => {
+    openModal();
+  });
+});
